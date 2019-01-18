4b46e86ab920f7959e7c86ac7fbe202ea288bed3
PhilipBox <jihwanla17@naver.com>
2019-01-17 19:27:47 -0800

    AJ exercise

---
+++ 2_AJ/5_array.cpp
@@ -0,0 +1,44 @@
+#include <cstdio>
+#include <algorithm>
+using namespace std;
+
+/*
+
+ 입력 : 5
+1 2 4 7 11
+3 5 8 12
+6 9 13
+10 14
+15
+
+ */
+
+
+int main(void)
+{
+
+    int num[100];
+    int n, total;
+
+    scanf("%d", &n);
+    total = (n*(n+1))/2;
+
+    // 입력받은 n줄만큼 출력될 전체 숫자의 수 배열에 입력
+    for(int i =0; i<total; i++){
+        num[i] = i+1;
+    }
+
+    int a=0, b=1, c=0, j=0;
+    for(int i=0; i<n; i++){ //4번의 개행
+        c=a;
+        for(j=i;j<n;j++) { // 한줄에 출력되는 숫자들
+            printf("%d ", num[c+j]);
+            c=c+j;
+        }
+        a = a+b;
+        b++;
+        printf("\n");
+
+    }
+    return 0;
+}
