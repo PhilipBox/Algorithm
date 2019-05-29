d00b84ac69765a150f5397525754103c6c78b507
PhilipBox <jihwanla17@naver.com>
2019-05-29 23:03:43 +0900

    0529 Algorithm

---
+++ 1_Baekjoon/3053_택시기하학.cpp
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <math.h>
+
+int main(void){
+
+    double r;
+
+    scanf("%lf", &r);
+
+
+    printf("%.6lf\n", M_PI*r*r);
+    printf("%.6lf\n", 2*r*r);
+
+
+    return 0;
+
+}
